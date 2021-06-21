9f476683a0c1ea929df4bdade2774b7ce0d471d3
Joseph Mutua <mutuaj793@gmail.com>
2021-06-21 18:05:57 +0300

    Add the index, movie and search routers

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const helmet = require("helmet");
 
+//Now_Playing
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+//Movies
+const movieRouter = require("./routes/movie");
+//Search
+const searchRouter = require("./routes/search");
 
 var app = express();
 app.use(helmet());
@@ -22,7 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/movie", movieRouter);
+app.use("/search", searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/movie.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET movie page */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Homepage" });
+});
+
+module.exports = router;
