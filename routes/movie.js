0b9f8a88a73d39e57b4a4bb4a8942bb4b2a02459
Joseph Mutua <mutuaj793@gmail.com>
2021-06-23 22:05:09 +0300

    Add movie rating route

--- routes/movie.js
+++ routes/movie.js
@@ -3,6 +3,34 @@ const router = express.Router();
 
 const movieDetails = require("../data/movieDetails");
 
+function requireJSON(req, res, next) {
+  if (!req.is("application/json"))
+    res.json({ msg: "Content type must be application/json" });
+  res.json("Test");
+}
+
+router.param("movieId", (req, res, next) => {
+  //Update the db with analytics data
+
+  console.log("Someone hit a route that used the movieId wildcard");
+  next();
+});
+
+router.get("/top_rated", (req, res, next) => {
+  let page = req.query.page;
+
+  if (!page) {
+    page = 0;
+  }
+
+  const results = movieDetails.sort((a, b) => {
+    return b.vote_average - a.vote_average;
+  });
+
+  let indexToStart = (page - 1) * 20;
+  res.json(results.slice(indexToStart, indexToStart + 20));
+});
+
 /* GET movie page */
 router.get("/:movieId", (req, res, next) => {
   const movieId = req.params.movieId;
@@ -15,4 +43,16 @@ router.get("/:movieId", (req, res, next) => {
     return res.json({ msg: "The movie you searched for cannot be found" });
   res.json(results);
 });
+
+router.post("/:movieId/rating", requireJSON, (req, res, next) => {
+  const movieId = req.params.movieId;
+
+  const userRating = req.body.value;
+
+  if (userRating < 0.5 || userRating > 10)
+    res.json({ msg: "Rating must be betweeen .5 and 10" });
+
+  res.json({ msg: "Thank you for submitting your rating" });
+});
+
 module.exports = router;
